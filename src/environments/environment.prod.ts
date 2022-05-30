ddbd5bca3f2b2992ba4c561d8f29c5276e67ee4f
Hein Htet Aung <heinhtetaung.mdy24@gmail.com>
2022-05-30 15:33:45 +0630

    final commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   environmentName: 'PRODUCTION',
-  API_BASE: 'http://172.104.40.242:4100/api',
-  UI_URL: 'http://172.104.40.242:4200',
+  API_BASE: 'http://localhost:8080/StudentRecApi/api',
+  UI_URL: 'http://localhost:4200',
   Version: '1.0.0',
 };
